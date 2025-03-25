6736cfdb72dafc0db787c6a76a5bc49145f458af
yean1234 <peaseoyean@gmail.com>
2025-03-26 04:21:52 +0900

    style:메뉴바 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from "styled-components";
 import avatar from './assets/w3school/avatar2.png';
+//import styles from './css/App.css';
+
 
 
 // styled component
@@ -11,8 +13,8 @@ const Menu = styled.div`
   @media (min-width: 998px){
     display: flex;
     flex-direction: column;
-    width: 20vw;
-    height: 100vh;
+    width: 300px;
+    height: 121px;
     background-color: white;
   }
   
@@ -49,26 +51,31 @@ const Logo = styled.div`
 const Welcome = styled.div`
   font-size:15px;
   padding-top: 15px;
+  flex-grow:0.5;
   
   
 `
 const LetternIcon = styled.div`
   display:flex;
   flex-direction:column;
-  flex-grow:1;
   justify-content:center;
-  padding-left:10px;
   align-items: center;
 `
 
 const Icon = styled.div`
   display:flex;
   flex-direction:row;
-  flex-grow:1;
-  justify-content:center;
-  align-items: center;
+  font-size: 15px;
+  padding-left:30px;
+  `
+const ProfileIcon = styled.div`
+  padding: 12px 16px 8px 16px;
+  &:hover {
+    background: rgb(220, 220, 220);
+    color: white;
+    transition: 0.5s;
+  }
 `
-
 const ProfilenLetter = styled.div`
   display:flex;
   flex-direction:row;
@@ -77,12 +84,94 @@ const ProfilenLetter = styled.div`
 `
 
 const ProfileWrapper = styled.div`
-  width: 50px;
-  height: 50px;
+  width: 43px;
+  height: 45px;
   border-radius: 50%;
-  margin-top: 10px;
-  flex-grow:0;
+  margin-top: 20px;
+  object-fit: contain;
+  padding-right:10px;
+
   
+`
+const DashContainer = styled.div`
+  display:flex;
+  flex-direction: column;
+
+`
+const Title = styled.div`
+  margin-top:15px;
+  font-size: 20px;
+  padding-left: 20px;
+`
+const OverviewContainer = styled.div`
+  margin-top:10px;
+  width: 100%;
+  height: 36px; 
+  display:flex;
+  flex-direction:row;
+  color: white;
+  background-color: #2196F3;
+  align-items: center;
+  &:hover {
+    background: rgb(220, 220, 220);
+    color: black;
+    transition: 0.2s;
+    cursor: pointer;
+  }
+
+`
+const BaseDashContainer = styled.div`
+  width: 100%;
+  height: 36px; 
+  display:flex;
+  flex-direction:row;
+  color: black;
+  align-items: center;
+  &:hover {
+    background: rgb(220, 220, 220);
+    color: black;
+    transition: 0.2s;
+    cursor: pointer;
+  }
+`
+const DashIcon = styled.div`
+  font-size: 18px;
+  margin-left: 10px;
+  margin-right:10px;
+`
+/*Gray board*/
+const GrayTitle = styled.div`
+  font-size: 20px;
+  
+`
+
+const GrayTitleContainer = styled.div`
+  display:flex;
+  margin-top:30px;
+  flex-direction:row;
+  align-items:center;
+`
+const GrayTitleIcon = styled.div`
+  font-size: 22px;
+  margin-left: 10px;
+  margin-right:10px;
+`
+const BigsquareContainer = styled.div`
+  display:flex;
+  margin-top: 10px;
+  width:100%;
+  height:17%;
+  flex-direction:row;
+  gap: 15px;
+  justify-content:center;
+  
+`
+const Square = styled.div`
+  width:23%;
+  height:100%;
+  background-color:teal;
+  background-color:${(props) => props.squareColor};
+ 
 `
 
 function App() {
@@ -105,20 +194,111 @@ function App() {
               Welcome, <b>Mike</b>
             </Welcome>
             <Icon>
-              <i class="fa fa-envelope"></i>
-              <i class="fa fa-users fa-fw"></i>
-              <i class="fa fa-cog"></i>
+              <ProfileIcon>
+                <i class="fa fa-envelope" ></i>
+              </ProfileIcon>
+              <ProfileIcon>
+                <i class="fa fa-users fa-fw"></i>
+              </ProfileIcon>
+              <ProfileIcon>
+                <i class="fa fa-cog"></i>
+              </ProfileIcon>
 
-            </Icon>
+            </Icon>    
             
           </LetternIcon>
           
         </ProfilenLetter>
         
+        <DashContainer>
+        <hr style={{ 
+          width: "300px", 
+          height: '0.4px',
+          background: "gray", 
+          border: '0', 
+          marginTop: '30px' }} />
+          <Title>Dashboard </Title>
+
+          <OverviewContainer>
+            <DashIcon>
+              <i class="fa fa-users fa-fw"></i>
+            </DashIcon>
+            Overview
+          </OverviewContainer>
+          <BaseDashContainer>
+            <DashIcon>
+              <i class="fa fa-eye fa-fw"></i>
+            </DashIcon>
+            Views
+          </BaseDashContainer>
+          <BaseDashContainer>
+            <DashIcon>
+              <i class="fa fa-users fa-fw"></i>
+            </DashIcon>
+            Traffic
+          </BaseDashContainer>
+          <BaseDashContainer>
+            <DashIcon>
+              <i class="fa fa-bullseye fa-fw"></i>
+            </DashIcon>
+            Geo
+          </BaseDashContainer>
+          <BaseDashContainer>
+            <DashIcon>
+              <i class="fa fa-diamond fa-fw"></i>
+            </DashIcon>
+            Orders
+          </BaseDashContainer>
+          <BaseDashContainer>
+            <DashIcon>
+              <i class="fa fa-bell fa-fw"></i>
+            </DashIcon>
+            News
+          </BaseDashContainer>
+          <BaseDashContainer>
+            <DashIcon>
+              <i class="fa fa-bank fa-fw"></i>
+            </DashIcon>
+            General
+          </BaseDashContainer>
+          <BaseDashContainer>
+            <DashIcon>
+            <i class="fa fa-history fa-fw"></i>
+            </DashIcon>
+            History
+          </BaseDashContainer>
+          <BaseDashContainer>
+            <DashIcon>
+              <i class="fa fa-cog fa-fw"></i>
+            </DashIcon>
+            Settings
+          </BaseDashContainer>
+
+        </DashContainer>
+        
+       
 
       </Menu>
-      <GrayBoard>
 
+
+      <GrayBoard>
+        <GrayTitleContainer>
+          <GrayTitleIcon>
+            <i class="fa fa-dashboard"></i>
+          </GrayTitleIcon>
+          <GrayTitle>
+            My Dashboard
+          </GrayTitle>
+        </GrayTitleContainer>
+        <BigsquareContainer>
+          <Square squareColor='rgb(244, 67, 54)'>
+            
+          </Square>
+          <Square squareColor='rgb(33, 150, 243)' />
+          <Square squareColor='rgb(0, 150, 136)'/>
+          <Square squareColor='rgb(255, 152, 0)'/>
+        </BigsquareContainer>
+        
       </GrayBoard>
     </MenunBoard>
     
