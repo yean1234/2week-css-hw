a11f0a00abd49a69c7d6f1d3713cf59a4452361a
yean1234 <peaseoyean@gmail.com>
2025-03-27 00:58:19 +0900

    style:지도 완성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import avatar from './assets/w3school/avatar2.png';
+import Map from './assets/w3school/region.jpg';
 //import styles from './css/App.css';
 
 
@@ -185,7 +186,7 @@ const SquareIconContainer = styled.div`
 `
 
 const SquareIcon = styled.div`
-  font-size: 3em;
+  font-size: 45px;
   margin-left: 10px;
   margin-right:10px;
 `
@@ -193,8 +194,53 @@ const SquareLetter = styled.div`
   padding-right:15px;
   font-size:30px;
 `
-
-
+const RegionContainer = styled.div`
+  display:flex;
+  flex-direction:row;
+  padding:20px;
+`
+const MiddleTitle = styled.div`
+  font-size:20px;
+  font-weight: lighter;
+  flex-grow:  ${(props) => (props.flexGrow ? props.flexGrow : "unset")};
+`
+const MapContainer = styled.div`
+  display:flex;
+  flex-direction: row;
+`
+const FeedsContainer = styled.div`
+  display:flex;
+  flex-grow:1;
+  flex-direction:column;
+  padding-right:20px;
+  padding-left:20px;
+  width:auto;
+`
+const FeedsElementContainer = styled.div`
+  display:flex;
+  flex-direction:row;
+  background-color:  ${(props) => (props.backcolor? props.backcolor : "unset")};
+  width:100%;
+  height:35px;
+  align-items:center;
+`
+const FeedsIcon = styled.div`
+  flex-grow:${(props) => (props.iconGrow ? props.iconGrow : "1")};
+  font-size:18px;
+  padding-left:${(props) => (props.paddingLeft ? props.paddingLeft : "20px")};
+  color: ${(props) => props.iconColor};
+`
+const FeedsMiddle = styled.div`
+  flex-grow:${(props) => (props.middleGrow? props.middleGrow : "3")};
+  font-size:17px;
+  font-weight: lighter;
+`
+const FeedsMins = styled.div`
+  flex-grow:2;
+  font-size:15px;
+  font-weight: lighter;
+  font-style: oblique;
+`
 function App() {
   return (
   <div>
@@ -344,9 +390,102 @@ function App() {
               margin: '0px'
               }}>Views</p>
           </Square>
-          <Square squareColor='rgb(0, 150, 136)'/>
-          <Square squareColor='rgb(255, 152, 0)'/>
+          <Square squareColor='rgb(0, 150, 136)'>
+            <SquareIconContainer>
+              <SquareIcon>
+                <i class="fa fa-share-alt w3-xxxlarge"></i>
+              </SquareIcon>
+              <SquareLetter>
+                23
+              </SquareLetter>
+            </SquareIconContainer>
+            <p style={{
+              fontSize: '20px', 
+              paddingLeft: '10px',
+              color: 'white',
+              margin: '0px'
+              }}>Shares</p>
+          </Square>
+          <Square squareColor='rgb(255, 152, 0)'>
+            <SquareIconContainer>
+              <SquareIcon>
+                <i class="fa fa-users w3-xxxlarge"></i>
+              </SquareIcon>
+              <SquareLetter>
+                50
+              </SquareLetter>
+            </SquareIconContainer>
+            <p style={{
+              fontSize: '20px', 
+              paddingLeft: '10px',
+              color: 'white',
+              margin: '0px'
+              }}>users</p>
+          </Square>
         </BigsquareContainer>
+        <RegionContainer>
+          <MiddleTitle flexGrow='1'>
+            Regions
+          </MiddleTitle>
+          <MiddleTitle flexGrow='2.5'>
+            Feeds
+          </MiddleTitle>
+        </RegionContainer>
+        <MapContainer>
+          <img src={Map} alt="Map" style={{ paddingLeft: '20px', width: "31%", height: "auto" }} />
+          <FeedsContainer>
+            <FeedsElementContainer backcolor='white'>
+              <FeedsIcon iconColor='rgb(33, 150, 243)'>
+                <i class="fa fa-user w3-text-blue w3-large"></i>
+              </FeedsIcon>
+              <FeedsMiddle>New record, over 90 views.</FeedsMiddle>
+              <FeedsMins>10 mins</FeedsMins>
+            </FeedsElementContainer>
+            <FeedsElementContainer >
+              <FeedsIcon iconColor='rgb(244, 67, 54)'>
+                <i class="fa fa-bell w3-text-red w3-large"></i>
+              </FeedsIcon>
+              <FeedsMiddle middleGrow='4.3'>Database error.</FeedsMiddle>
+              <FeedsMins>15 mins</FeedsMins>
+            </FeedsElementContainer>
+            <FeedsElementContainer backcolor='white' >
+              <FeedsIcon iconColor='rgb(210, 190, 14)' iconGrow='0.95' paddingLeft='17px'>
+                <i class="fa fa-users w3-text-yellow w3-large"></i>
+              </FeedsIcon>
+              <FeedsMiddle >New record, over 40 users.</FeedsMiddle>
+              <FeedsMins>17 mins</FeedsMins>
+            </FeedsElementContainer>
+            <FeedsElementContainer >
+              <FeedsIcon iconColor='rgb(244, 67, 54)' iconGrow='1.01' >
+                <i class="fa fa-comment w3-text-red w3-large"></i>
+              </FeedsIcon>
+              <FeedsMiddle middleGrow='4.2' >New comments.</FeedsMiddle>
+              <FeedsMins>25 mins</FeedsMins>
+            </FeedsElementContainer>
+            <FeedsElementContainer backcolor='white' >
+              <FeedsIcon iconColor='rgb(33, 150, 243)' paddingLeft='23px' iconGrow='1.02'>
+                <i class="fa fa-bookmark w3-text-blue w3-large"></i>
+              </FeedsIcon>
+              <FeedsMiddle middleGrow='3.8' >Check transactions.</FeedsMiddle>
+              <FeedsMins>28 mins</FeedsMins>
+            </FeedsElementContainer>
+            <FeedsElementContainer >
+              <FeedsIcon iconColor='rgb(244, 67, 54)' paddingLeft='19px'  iconGrow='0.95'>
+                <i class="fa fa-laptop w3-text-red w3-large"></i>
+              </FeedsIcon>
+              <FeedsMiddle middleGrow='4.3' >CPU overload.</FeedsMiddle>
+              <FeedsMins>35 mins</FeedsMins>
+            </FeedsElementContainer>
+            <FeedsElementContainer  backcolor='white' >
+              <FeedsIcon iconColor='rgb(76, 175, 80)' paddingLeft='22px'  >
+                <i class="fa fa-share-alt w3-text-green w3-large"></i>
+              </FeedsIcon>
+              <FeedsMiddle middleGrow='4.4' >New shares.</FeedsMiddle>
+              <FeedsMins>39 mins</FeedsMins>
+            </FeedsElementContainer>
+          </FeedsContainer>
+        </MapContainer>
+        
         
       </GrayBoard>
     </MenunBoard>
